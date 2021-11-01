a32a0cf22bdec8338ea412dc36946546c31eca15
Sagar Chaudhary <79923846+sagar2022@users.noreply.github.com>
2021-11-01 21:53:43 +0545

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const people = [
+  {
+    id: 1,
+    image: "https://avatars.githubusercontent.com/u/79923846?v=4",
+    name: "Sagar anbu",
+    title: "the boss",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache."
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami."
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo."
+  },
+  {
+    id: 4,
+    image: "https://avatars.githubusercontent.com/u/42583621?v=4",
+    name: "sanjiv chaudhary",
+    title: "office manager",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. "
+  }
+];
+
+export default people;
